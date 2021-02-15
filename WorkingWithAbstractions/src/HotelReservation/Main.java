35ef3957c0ab357bac82663f3e38d14d40c4c15e
Polina Pavlova <poli.paskaleva@gmail.com>
2021-02-15 15:40:21 +0200

    JavaOOP Homeworks

--- WorkingWithAbstractions/src/HotelReservation/Main.java
+++ WorkingWithAbstractions/src/HotelReservation/Main.java
@@ -10,7 +10,6 @@ class Main {
         Scanner scanner = new Scanner (System.in);
         String[] input = scanner.nextLine ().split ("\\s+");
         double pricePerDay = Double.parseDouble (input[0]);
-
         int days = Integer.parseInt (input[1]);
         String seasonName = input[2].toUpperCase ();
         Season season = Season.valueOf (seasonName);
