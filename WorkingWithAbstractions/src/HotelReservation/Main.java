280ff2b3ba7ead42348105d6ab5856f1dce80b27
Polina Pavlova <poli.paskaleva@gmail.com>
2021-02-15 15:21:10 +0200

    JavaOOP Homeworks

--- WorkingWithAbstractions/src/HotelReservation/Main.java
+++ WorkingWithAbstractions/src/HotelReservation/Main.java
@@ -8,7 +8,6 @@ class Main {
     public static
     void main (String[] args) {
         Scanner scanner = new Scanner (System.in);
-
         String[] input = scanner.nextLine ().split ("\\s+");
 
         double pricePerDay = Double.parseDouble (input[0]);
