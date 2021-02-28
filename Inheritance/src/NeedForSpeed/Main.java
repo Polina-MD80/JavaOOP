71e2bd286f7053ebdb357ccadf7561bab1256aae
Polina Pavlova <poli.paskaleva@gmail.com>
2021-02-28 22:02:08 +0200

    JavaOOP Homeworks

---
+++ Inheritance/src/NeedForSpeed/Main.java
@@ -0,0 +1,9 @@
+package NeedForSpeed;
+
+public
+class Main {
+    public static
+    void main (String[] args) {
+
+    }
+}
---
+++ Inheritance/src/hero/Main.java
@@ -0,0 +1,11 @@
+package hero;
+
+import NeedForSpeed.SportCar;
+
+public
+class Main {
+    public static
+    void main (String[] args) {
+
+    }
+}
