7c45491582d1d60a90a3bd2aff0a590e8f0d2fff
Polina Pavlova <poli.paskaleva@gmail.com>
2021-03-03 18:13:57 +0200

    JavaOOP Homeworks

---
+++ InterfacesAndAbstraction/src/Telephony/Main.java
@@ -0,0 +1,26 @@
+package Telephony;
+
+import java.util.List;
+import java.util.Scanner;
+
+public
+class Main {
+    public static
+    void main (String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+
+        List<String> numbers = List.of (scanner.nextLine ().split ("\\s+"));
+        List<String> urls    = List.of (scanner.nextLine ().split ("\\s+"));
+
+        Smartphone smartphone = new Smartphone (numbers,urls);
+
+
+
+        System.out.print (smartphone.call ());
+        System.out.println (smartphone.browse ());
+
+
+    }
+}
---
+++ InterfacesAndAbstraction/src/Telephony/Smartphone.java
@@ -0,0 +1,76 @@
+package Telephony;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public
+class Smartphone implements Callable, Browsable {
+    private
+    List<String> numbers;
+    private List<String> urls;
+
+    public
+    Smartphone (List<String> numbers, List<String> urls) {
+        acceptNumbers (numbers);
+        acceptURLs (urls);
+    }
+
+    private
+    void acceptNumbers (List<String> numbers) {
+        this.numbers = new ArrayList<> ();
+        for (String number : numbers) {
+            boolean numberIsValid = true;
+            for (char c : number.toCharArray ()) {
+                if (!Character.isDigit (c)) {
+                    this.numbers.add ("Invalid number!");
+                    numberIsValid = false;
+                    break;
+                }
+            }
+            if (numberIsValid) {
+                this.numbers.add (number);
+            }
+
+        }
+    }
+
+    private
+    void acceptURLs (List<String> urls) {
+        this.urls = new ArrayList<> ();
+        for (String url : urls) {
+            boolean urlIsValid = true;
+            for (char c : url.toCharArray ()) {
+                if (Character.isDigit (c)) {
+                    this.urls.add ("Invalid URL!");
+                    urlIsValid = false;
+                    break;
+                }
+            }
+            if (urlIsValid) {
+                this.urls.add (url);
+            }
+
+        }
+    }
+
+    @Override
+    public
+    String call () {
+        StringBuilder sb = new StringBuilder ();
+        for (String number : numbers) {
+            sb.append ("Calling... ").append (number).append (System.lineSeparator ());
+        }
+        return sb.toString ();
+    }
+
+    @Override
+    public
+    String browse () {
+        StringBuilder sb = new StringBuilder ();
+        for (String url : urls) {
+            sb.append ("Browsing: ").append (url).append (System.lineSeparator ());
+        }
+        return sb.toString ();
+    }
+
+}
