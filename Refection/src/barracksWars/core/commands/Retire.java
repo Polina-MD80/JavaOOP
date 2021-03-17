4834668ad2e01f13048b4b87866bc364858764c5
Polina Pavlova <poli.paskaleva@gmail.com>
2021-03-17 15:53:11 +0200

    JavaOOP Homeworks

---
+++ Refection/src/barracksWars/core/commands/Retire.java
@@ -0,0 +1,27 @@
+package barracksWars.core.commands;
+
+import barracksWars.interfaces.Repository;
+import barracksWars.interfaces.UnitFactory;
+import jdk.jshell.spi.ExecutionControl;
+
+public
+class Retire extends Command {
+    public
+    Retire (String[] data, Repository repository, UnitFactory unitFactory) {
+        super (data, repository, unitFactory);
+    }
+
+    @Override
+    public
+    String execute () {
+          String result;
+        try {
+            getRepository ().removeUnit (getData ()[1]);
+           result = getData ()[1] + " retired!";
+        } catch (ExecutionControl.NotImplementedException e) {
+         result =  e.getMessage ();
+
+        }return result;
+
+    }
+}
