fac6235adafb87ed76b7e6b0e47fef815aaca7cb
Polina Pavlova <poli.paskaleva@gmail.com>
2021-03-18 16:42:18 +0200

    JavaOOP Homeworks

--- ExceptionsAndErrorHandling/src/PersonMain.java
+++ ExceptionsAndErrorHandling/src/PersonMain.java
@@ -3,7 +3,7 @@ class PersonMain {
     public static
     void main (String[] args) {
         try {
-            Person person = new Person ("", "Ivanov", 12);
+            Person person = new Person ("Ivan", "Ivanov", 150);
         }catch (IllegalArgumentException ex){
             System.out.println (ex.getMessage ());
         }
